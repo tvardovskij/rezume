616c9849e2dd5ebf39d3222c86f3c135d8957b02
Dmitry <dmitrijgusenkov74@gmail.com>
2026-04-02 21:30:30 +0300

    analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { App } from '@/app/App'
+import { Analytics } from "@vercel/analytics/react"
 import '@/app/styles/index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Analytics />
     <App />
   </StrictMode>,
 )
